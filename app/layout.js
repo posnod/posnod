6d4e97b8722e5e0a04a4b72de20f68dc101a3ecc
rikchooo <hermawanrikcho@gmail.com>
2025-11-24 23:22:23 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -80,6 +80,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   );
