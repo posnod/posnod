c4960014378ae86b61de572cd0126ececea4285d
rikchooo <hermawanrikcho@gmail.com>
2025-11-25 13:51:20 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -23,16 +23,16 @@ export const metadata = {
 
   icons: {
     icon: [
-      { url: '/Favicon.ico' },
+      { url: '/favicon.ico' },
       { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-      { url: '/Favicon-192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/Favicon-512.png', sizes: '512x512', type: 'image/png' }
+      { url: '/favicon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/favicon-512.png', sizes: '512x512', type: 'image/png' }
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
     ],
-    shortcut: ['/Favicon.ico']
+    shortcut: ['/favicon.ico']
   },
 
   openGraph: {
