a99954d758ea0bdff333b870332a5c07a7bd9dab
rikchooo <hermawanrikcho@gmail.com>
2025-11-27 12:56:27 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,7 @@ export const metadata = {
   },
 
   description:
-    "Platform pembelajaran sosial yang menyediakan pondasi modern untuk belajar, berbagi pengetahuan, dan berkolaborasi dalam skala luas.",
-
-  applicationName: "Posnod",
+    "Platform pembelajaran sosial yang menyediakan pondasi modern untuk belajar, berbagi pengetahuan, dan berkolaborasi.",
 
   keywords: [
     "Posnod",
@@ -24,13 +22,18 @@ export const metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png"},
-      { url: "/favicon-16x16.png", sizes:"16x16", type: "image/png"}
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
-    shortcut: ["/favicon.ico"],
+  },
+
+  manifest: "/manifest.json",
+
+  alternates: {
+    canonical: "https://posnod.com",
   },
 
   robots: {
@@ -43,13 +46,7 @@ export const metadata = {
       maxImagePreview: "large",
       maxSnippet: -1,
     },
-  },
-
-  manifest: "/manifest.json",
-
-  alternates: {
-    canonical: "https://posnod.com",
-  },
+  }
 };
 
 export const viewport = {
@@ -60,14 +57,6 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      </head>
       <body className="bg-[#FBFBFB]">
         <main>{children}</main>
       </body>
