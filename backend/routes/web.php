c0efa58bf12590ad6b47fef4305e867480582c1a
abdul mukti <abdulmukti40390@gmail.com>
2026-01-05 15:20:07 +0700

    fix(auth): implement Google OAuth login (dev)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/auth/{provider}/redirect', [SocialAuthController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [SocialAuthController::class, 'callback']);
