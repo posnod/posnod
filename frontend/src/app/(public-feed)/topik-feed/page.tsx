2570e5b453ca229d67bcfad6773dc2f70632172e
mardin <tredingb25@gmail.com>
2026-01-11 13:10:15 +0700

    feat: restructure TopikFeed component for improved layout and styling

--- frontend/src/app/(public-feed)/topik-feed/page.tsx
+++ frontend/src/app/(public-feed)/topik-feed/page.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const TopikFeed = () => {
   return (
     <div className='max-w-7xl mx-auto'>
-      <h1 className='capitalize text-xl'>topk</h1>
+      <div className='flex items-center gap-6 mt-8'>
+        <h1 className='text-h2 capitalize'>topik</h1>
+        <div className='flex items-center gap-2'>
+          <div className='h-8 w-8 rounded-full bg-[#38BE7E]' />
+          <h1 className='text-h3'>Psikologi & Kepribadian</h1>
+        </div>
+      </div>
     </div>
   )
 }
